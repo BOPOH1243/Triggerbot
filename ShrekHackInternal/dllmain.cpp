9d110724eabc1729b3e0b91429f1f5f238c8d768
BOPOH1243 <tenpenni@mail.ru>
2022-09-20 06:12:56 +0500

    Starting

---
+++ ShrekHackInternal/dllmain.cpp
@@ -0,0 +1,26 @@
+﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
+#include "pch.h"
+#include "include.h"
+
+BOOL WINAPI triggerThread(HMODULE hModule) {
+
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        DisableThreadLibraryCalls(hModule);
+        CloseHandle(CreateThread(0, 0, (LPTHREAD_START_ROUTINE)triggerThread, 0, 0, 0));
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
---
+++ ShrekHackInternal/include.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <thread>
+#include <string>
+#include <chrono>
+
+#include "offsets.h"
+#include "Windows.h"
+using namespace offsets::netvars;
+using namespace offsets::signatures;
